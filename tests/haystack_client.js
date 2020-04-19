aa4e366f625f8b4b1d43d838fe2b7d292e01beb6
Matthew Foley <mtfoley.mae@gmail.com>
2020-04-19 09:16:43 -0400

    Add isLoggedIn() to Haystack Client Test

--- tests/haystack_client.js
+++ tests/haystack_client.js
@@ -3,27 +3,16 @@ const { Collector } = require("../src/collector");
 const assert = require('assert');
 const nock = require('nock');
 const config = require('config');
+// Setup all testing parameters
 const testIdZinc = "@testId";
 const testIdJson = "r:testId"
-/*
-    "haystack": {
-        "protocol": "https",
-        "host": "haystack.org",
-        "project":"project",
-        "port": 443,
-        "authentication": {
-            "username": "username",
-            "password": "password"    
-        },
-    },
-*/
 const testTrend = {id:'collector-test',source:'test'};
 const date = new Date();
 const testData = [
   {ts:date.getTime(),val:0.5},
   {ts:date.getTime()+5,val:0.7}
 ];
-
+// function for Haystack API Mocking
 const makeNockScopes = function(){
     const hcfg = config.get("haystack");
     let host = `${hcfg.protocol}://${hcfg.host}:${hcfg.port}`;
@@ -51,6 +40,7 @@ const makeNockScopes = function(){
             200,
             'ver: "3.0"\nempty\n'
         );
+    // authScope1, authScope2, and authScope3 handle the server side of Haystack Authentication
     const authScope1 = nock(host,{
         reqheaders:{
             'Authorization': /HELLO/gi
@@ -154,14 +144,11 @@ describe('HaystackClient', function() {
             });
         });
     });
-    describe('#_makeHistoryGrid(id,data)',function(){
-        it('works');
-    });
-    describe('#_makeCommitGrid(data)',function(){
-        it('works');
-    });
-    describe('#_makePath(path',function(){
-        it('works');
+    describe('#isLoggedIn()',function(){
+        it('returns true after successful connection',function(done){
+            assert(haystackClient.isLoggedIn() == true);
+            done();
+        });
     });
     describe('destroy()',function(){
         it('cancels pingJob and pushJob',function(){
@@ -171,116 +158,4 @@ describe('HaystackClient', function() {
             assert.ok(cancelled);
         });
     });
-/*
-    _makeCommitGrid(data){
-        const grid = [
-            "ver: \"3.0\" commit:\"add\""
-        ];
-        let config = {
-            point:"M",
-            dis:data.name,
-            his:"M",
-            tz:"New_York",
-            kind:"Number"
-        };
-        if(data.units !== null){
-            const key = ""+data.units;
-            if(haystackUnits.hasOwnProperty(key) == true) config.unit = haystackUnits[key];
-        }
-        grid.push(Object.keys(config).join(','));
-        grid.push(this._encodeZinc(config));
-        return grid.join("\n");
-    }
-    _makePath(path){
-        return this.options.protocol+'://'+this.options.host+':'+this.options.port+'/api/'+this.options.project+'/'+path;
-    }
-    _postGrid(path,grid){
-        return new Promise((resolve,reject)=>{
-            axios.post(path,grid).then((response)=>{
-                resolve(this._decodeJson(response.data));
-            }).catch((error)=>{
-                reject(error);
-            });    
-        });
-    }
-    isLoggedIn(){
-        return this.authToken !== null;
-    }
-    destroy(){
-        logger.info('Haystack Client Destroy');
-        if(this.pingJob) this.pingJob.cancel();
-        this.removeAllListeners();
-    }
-    ping(){
-        const self = this;
-        axios.get(self.paths.ABOUT).then((response)=>{
-            const data = this._decodeJson(response.data);
-            logger.info('Haystack Ping');
-            self.emit('pingSuccess',data);
-        }).catch(()=>{
-            self.authToken = null;
-            self.connect();
-        });
-    }
-    hisRead(id,range){
-        const self = this;
-        const grid = [
-            "ver: \"3.0\"",
-            "id,range",
-            `${id},"${range}"`
-        ].join('\n');
-        return new Promise((resolve,reject)=>{
-            axios.post(self.paths.HIS_READ,grid).then((response)=>{
-                resolve(this._decodeJson(response.data));
-            }).catch((error)=>{
-                reject(error);
-            })
-        })
-    }
-    connect(){
-        const self = this;
-        return new Promise((resolve,reject)=>{
-            dns.lookup(self.options.host,(err)=>{
-                if(err){
-                    reject('DNS Error: '+err);
-                } else {
-                    if( !self.isLoggedIn() ){
-                        let context = null;
-                        try {
-                            context = new haystackAuth.AuthClientContext(
-                                self.paths.ABOUT,
-                                self.options.authentication.username,
-                                self.options.authentication.password,
-                                (self.options.protocol=='https')
-                            );    
-                        } catch(error){
-                            reject(error);
-                        }
-                        context.login((header)=>{
-                            self.authToken = header['Authorization'];
-                            axios.defaults.headers['Authorization'] = self.authToken;
-                            axios.defaults.headers.post['Content-Type'] = 'text/zinc; charset=utf-8';
-                            axios.defaults.headers['Accept'] = 'application/json';
-                            if(!self.pingJob) self.pingJob = scheduleJob(self.options.pingSchedule,()=>{
-                                self.ping();
-                            });
-                            self.emit('connected',header);
-                            resolve(header);
-                        },(error)=>{
-                            reject(error||'Authentication Error');
-                        });  
-                    }
-                }
-            });
-        });
-    }
-    commit(data){
-        const grid = this._makeCommitGrid(data);
-        return this._postGrid(this.paths.COMMIT,grid);
-    }
-    hisWrite(id,data){
-        const grid = this._makeHistoryGrid(id,data);
-        return this._postGrid(this.paths.HIS_WRITE,grid);
-    }
-*/
 });
